91d5ba1c0ea2bd673ad856cf201eecfb3d05d4c3
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-04-14 13:48:11 -0700

    Update TaxTrackerApp.java

--- src/main/java/com/example/taxtracker/TaxTrackerApp.java
+++ src/main/java/com/example/taxtracker/TaxTrackerApp.java
@@ -1,1 +1,8 @@
+// --- Application Entry Point ---
 
+@SpringBootApplication
+public class TaxTrackerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TaxTrackerApp.class, args);
+    }
+}
