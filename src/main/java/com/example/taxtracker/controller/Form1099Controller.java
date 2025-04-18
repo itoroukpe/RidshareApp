bd1b59bdcc8f46d170d0042d6b27f08e26ca5f53
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-04-18 07:55:00 -0700

    Create Form1099Controller.java

---
+++ src/main/java/com/example/taxtracker/controller/Form1099Controller.java
@@ -0,0 +1,20 @@
+@RestController
+@RequestMapping("/api/forms")
+public class Form1099Controller {
+    @Autowired private UserRepository userRepo;
+    @Autowired private IncomeRepository incomeRepo;
+    @Autowired private Form1099Service form1099Service;
+
+    @GetMapping("/1099/{userId}")
+    public ResponseEntity<byte[]> download1099(@PathVariable Long userId) throws IOException {
+        User user = userRepo.findById(userId).orElseThrow();
+        List<Income> incomes = incomeRepo.findByUserId(userId);
+        byte[] pdf = form1099Service.generate1099Form(user, incomes);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDisposition(ContentDisposition.attachment().filename("form-1099.pdf").build());
+
+        return ResponseEntity.ok().headers(headers).body(pdf);
+    }
+}
