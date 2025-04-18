74a09a9025dc2f636a45906d32d379a662cd0236
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-04-18 11:38:27 -0700

    Update UserRepository.java

--- src/main/java/com/example/taxtracker/repository/UserRepository.java
+++ src/main/java/com/example/taxtracker/repository/UserRepository.java
@@ -1,4 +1,8 @@
+package com.example.taxtracker.repository;
+
+import com.example.taxtracker.model.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 
