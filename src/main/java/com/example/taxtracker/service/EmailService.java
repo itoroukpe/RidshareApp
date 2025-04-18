42e4af7ab529b461d0eedf3ce1649b7c218b03d4
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-04-18 05:57:42 -0700

    Create EmailService.java

---
+++ src/main/java/com/example/taxtracker/service/EmailService.java
@@ -0,0 +1,12 @@
+@Service
+public class EmailService {
+    @Autowired private JavaMailSender mailSender;
+
+    public void sendOtp(String to, String otp) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("Your OTP Code");
+        message.setText("Use this code to complete your login: " + otp);
+        mailSender.send(message);
+    }
+}
