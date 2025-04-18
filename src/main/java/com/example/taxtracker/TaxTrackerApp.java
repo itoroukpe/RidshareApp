02b47535992f9769b16731dcfe4b657b9dcce318
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-04-18 11:24:53 -0700

    Update TaxTrackerApp.java

--- src/main/java/com/example/taxtracker/TaxTrackerApp.java
+++ src/main/java/com/example/taxtracker/TaxTrackerApp.java
@@ -1,7 +1,7 @@
 package com.example.taxtracker.service;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Service;
 
 // --- Application Entry Point ---
