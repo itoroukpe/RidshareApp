df77cb47f8fc16843989773478a9793034333ad2
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-04-18 06:02:03 -0700

    Update EmailService.java

--- src/main/java/com/example/taxtracker/service/EmailService.java
+++ src/main/java/com/example/taxtracker/service/EmailService.java
@@ -1,4 +1,10 @@
+// --- File: EmailService.java ---
 package com.rondus.taxtracker.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
