a88ec5096e42473319c54ddf8ddb633f65180de5
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-04-18 12:20:24 -0700

    Update ExpenseController.java

--- src/main/java/com/example/taxtracker/controller/ExpenseController.java
+++ src/main/java/com/example/taxtracker/controller/ExpenseController.java
@@ -2,6 +2,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import com.example.taxtracker.model.User;
+import com.example.taxtracker.model.Income;
+import com.example.taxtracker.model.Expense;
 
 
 // --- ExpenseController.java ---
