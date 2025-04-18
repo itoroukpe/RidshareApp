3b86a6c1ab69b36dbe5ec81f57673d7e2d820975
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-04-18 06:35:07 -0700

    Create SubscriptionController.java

---
+++ src/main/java/com/example/taxtracker/controller/SubscriptionController.java
@@ -0,0 +1,16 @@
+@RestController
+@RequestMapping("/api/subscription")
+public class SubscriptionController {
+    @Autowired private StripeService stripeService;
+
+    @GetMapping("/checkout")
+    public ResponseEntity<Map<String, String>> checkout() throws StripeException {
+        String successUrl = "http://localhost:8080/subscription-success.html";
+        String cancelUrl = "http://localhost:8080/subscription-cancel.html";
+        Session session = stripeService.createCheckoutSession(successUrl, cancelUrl);
+        Map<String, String> response = new HashMap<>();
+        response.put("id", session.getId());
+        response.put("url", session.getUrl());
+        return ResponseEntity.ok(response);
+    }
+}
