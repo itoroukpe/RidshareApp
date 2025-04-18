880e9a54823d3187d9ce399ae6a218cae5763fd5
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-04-18 06:34:18 -0700

    Create StripeService.java

---
+++ src/main/java/com/example/taxtracker/service/StripeService.java
@@ -0,0 +1,20 @@
+@Service
+public class StripeService {
+    static {
+        Stripe.apiKey = "sk_test_YourSecretKeyHere";
+    }
+
+    public Session createCheckoutSession(String successUrl, String cancelUrl) throws StripeException {
+        SessionCreateParams params = SessionCreateParams.builder()
+            .setMode(SessionCreateParams.Mode.SUBSCRIPTION)
+            .setSuccessUrl(successUrl)
+            .setCancelUrl(cancelUrl)
+            .addLineItem(
+                SessionCreateParams.LineItem.builder()
+                    .setQuantity(1L)
+                    .setPrice("price_YourStripePriceID")
+                    .build())
+            .build();
+        return Session.create(params);
+    }
+}
