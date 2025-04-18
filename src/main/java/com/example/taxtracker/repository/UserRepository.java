bd88decd3ddb4e63491157d099d3a3ada46fe160
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-04-18 11:26:27 -0700

    Update UserRepository.java

--- src/main/java/com/example/taxtracker/repository/UserRepository.java
+++ src/main/java/com/example/taxtracker/repository/UserRepository.java
@@ -1,3 +1,6 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
 // --- UserRepository.java ---
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
